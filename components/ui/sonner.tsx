ff73259ce3bcd30b25cc234ae79481a464c21bc0
Tristan <nocent.tristan@gmail.com>
2026-05-05 15:04:20 +0200

    setup auth layouts, social auth form, Next Auth, OAuth with Google and GitHub

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
